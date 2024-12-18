1ad053f6108ae8d1b565e334191923bf5f6758a9
Rafal Zygula <p0hmgxcn@students.codeinstitute.net>
2024-12-18 21:07:38 +0000

    Added cache control

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -183,17 +183,26 @@ USE_TZ = True
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),) #tuple brackets
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 if 'USE_AWS' in os.environ:
+    #Cache Control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
+
+
+
+
+
     # Bucket Configuration:
     AWS_STORAGE_BUCKET_NAME = 'butikadobucket'
     AWS_S3_REGION_NAME = 'eu-north-1'
