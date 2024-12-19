a51f2515d5c5249a6a6b9e52f0debdf604a5dcea
Rafal Zygula <p0hmgxcn@students.codeinstitute.net>
2024-12-19 08:08:02 +0000

    Add 404 page

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -18,3 +20,7 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+handler404 = 'boutique_ado.views.handler404'
+
